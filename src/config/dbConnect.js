b93bfc8f1edd90814c7a68365b93483704c133a9
Daniel Vilela <55055330+DanielVibuso@users.noreply.github.com>
2023-02-20 00:13:23 -0300

    Delete dbConnect.js

--- src/config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb+srv://daniel2bueno:n6fKHCXEYLoVlL8x@cluster0.lwaslvo.mongodb.net/nodeapp");
-
-let db = mongoose.connection;
-
-export default db;
